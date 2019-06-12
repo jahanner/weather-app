73f61389899306d084a5f5a0aa9279df5cf8f757
Jesse Hanner <hannerj@objectcomputing.com>
2019-06-12 15:56:56 -0500

    fixes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiamFoYW5uZXIiLCJhIjoiY2p3bWxnd3N4MDluaTQ4bW1hbXdyeXIzbCJ9.c3JUo3XM4keWFB8-9YCmsA&limit=1`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
