e1d72d61b4a91fb403b55cc83fc990ec27c95f3c
Jesse Hanner <hannerj@objectcomputing.com>
2019-08-23 16:48:08 -0500

    removed trailing zeros on precipPercent

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,13 @@ const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/3ca4d97668da62d29860ccde8ed12fd4/${latitude},${longitude}`
 
     request({ url, json: true }, (error, { body }) => {
+        let percent = body.daily.data[0].precipProbability * 100
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degress out and there is a ${body.daily.data[0].precipProbability * 100}% chance of rain. The high for today is ${body.daily.data[0].temperatureHigh} and the low is ${body.daily.data[0].temperatureLow}`)
+            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degress out and there is a ${percent.toFixed(0)}% chance of rain. The high for today is ${body.daily.data[0].temperatureHigh} and the low is ${body.daily.data[0].temperatureLow}`)
         }
     })
 }
